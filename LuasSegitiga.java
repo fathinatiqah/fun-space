f7d1eb1dbe85c0e49ebe1b7ce8a9eed4fc128eb3
fathinatiqah <fathinatiqahf@gmail.com>
2024-09-06 10:45:09 +0700

    First Github Project

---
+++ LuasSegitiga.java
@@ -0,0 +1,16 @@
+public class LuasSegitiga {
+
+    public static void main(String[] args) {
+        //deklarasi variabel
+        double alas, tinggi, luas;
+
+        //instalasi variabel
+        alas=5;
+        tinggi=10;
+        luas=0.5*alas*tinggi;
+
+        //cetak hasil
+        System.out.println("luas segitiga adalah "+luas);
+
+    }
+}
